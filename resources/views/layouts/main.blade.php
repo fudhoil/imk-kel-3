cc1830062306b940b8d3439eef9f46e0f1596088
fudhoil <71709328+fudhoil@users.noreply.github.com>
2022-01-20 20:21:14 +0700

    5.0

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,7 @@
       <link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   
       <!-- Custom styles for this template-->
-      <link href="css/sb-admin-2.min.css" rel="stylesheet">
+      <link href="#" rel="stylesheet">
   
   </head>
   <body id="page-top">
@@ -22,13 +22,13 @@
     <!-- Page Wrapper -->
     <div id="wrapper">
 
+      @include('partials.navbar')
       @include('partials.sidebar')
-      <!-- Content Wrapper -->
+      
       <div id="content-wrapper" class="d-flex flex-column">
         
         <!-- Main Content -->
         <div id="content">
-              @include('partials.navbar')
               @yield('container')
         </div>
         <!-- End of Main Content -->
@@ -53,30 +53,12 @@
       <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true"></span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js" integrity="sha512-0QbL0ph8Tc8g5bLhfVzSqxe9GERORsKhIn1IrpxDAgUsbBGz/V7iSav2zzW325XGd1OMLdL4UiqRJj702IeqnQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+   
 
 </body>
 </html>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -13,8 +13,8 @@
   <hr class="sidebar-divider my-0">
 
   <!-- Nav Item - Dashboard -->
-  <li class="nav-item active">
-      <a class="nav-link" href="index.html">
+  <li class="nav-item {{ ($title === "Home") ? 'active' : '' }}">
+      <a class="nav-link" href="/">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
   </li>
@@ -37,9 +37,9 @@
       <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
               <h6 class="collapse-header">Login Screens:</h6>
-              <a class="collapse-item" href="/login">Login</a>
-              <a class="collapse-item" href="/barang">Barang</a>
-              <a class="collapse-item" href="/peinjaman">Peminjaman</a>
+              <a class="collapse-item " href="/login">Login</a>
+              <a class="collapse-item " href="/barang">Barang</a>
+              <a class="collapse-item " href="/peinjaman">Peminjaman</a>
               <a class="collapse-item" href="/register">Register</a>
               <a class="collapse-item" href="/forgot-password">Forgot Password</a>
               <div class="collapse-divider"></div>
