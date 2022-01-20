81fbb2aa22b0502065a1baca7aae952d4e8b1f37
fudhoil <71709328+fudhoil@users.noreply.github.com>
2022-01-20 18:06:38 +0700

    2.4

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,23 +7,23 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <style>
+        html, body {
+            overflow-x: hidden;
+            height: 100%;
+        }
+    </style>
     <title>IMK | {{ $title }}</title>
   </head>
-  <body>
+  <body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
 
     @include('partials.navbar')
-    
-    <div class="container m-0 p-0">
-        <div class="row">
-            <div class="col">
-                @include('partials.sidebar')
-            </div>
-            <div class="col py-5">
-                @yield('container')
-            </div>
-        </div>
-    </div>
+    @include('partials.sidebar')
+
+    @yield('container')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
