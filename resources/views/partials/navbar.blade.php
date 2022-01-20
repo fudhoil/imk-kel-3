1c0e89182bc007228697e08f4a2b1975edea3c1f
fudhoil <71709328+fudhoil@users.noreply.github.com>
2022-01-20 08:53:15 +0700

    first commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-warning shadow">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <span class="badge bg-primary p-3">Navbar</span>
+    </a>
+  </div>
+</nav>
