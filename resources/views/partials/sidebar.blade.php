4a427bc33bac0964f1039f01ebe8d58b2648c985
fudhoil <71709328+fudhoil@users.noreply.github.com>
2022-01-20 19:27:06 +0700

    4.1

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -37,13 +37,15 @@
       <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
               <h6 class="collapse-header">Login Screens:</h6>
-              <a class="collapse-item" href="login.html">Login</a>
-              <a class="collapse-item" href="register.html">Register</a>
-              <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+              <a class="collapse-item" href="/login">Login</a>
+              <a class="collapse-item" href="/barang">Barang</a>
+              <a class="collapse-item" href="/peinjaman">Peminjaman</a>
+              <a class="collapse-item" href="/register">Register</a>
+              <a class="collapse-item" href="/forgot-password">Forgot Password</a>
               <div class="collapse-divider"></div>
               <h6 class="collapse-header">Other Pages:</h6>
-              <a class="collapse-item" href="404.html">404 Page</a>
-              <a class="collapse-item" href="blank.html">Blank Page</a>
+              <a class="collapse-item" href="/404-not-found">404 Page</a>
+              <a class="collapse-item" href="/blank-page">Blank Page</a>
           </div>
       </div>
   </li>
