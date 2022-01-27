f9a260f6bebf0a6cdd57ce8e3ce925bfa7a6bf01
fudhoil <71709328+fudhoil@users.noreply.github.com>
2022-01-28 01:06:12 +0700

    8.7.1 sisa edit

--- resources/views/barang.blade.php
+++ resources/views/barang.blade.php
@@ -59,6 +59,7 @@
                     </div>
                   </div>
                   <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
                     <button type="submit" class="btn btn-primary">Tambahkan</button>
                   </div>
               </div>
