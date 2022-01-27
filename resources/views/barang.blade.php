17a4413fc7fd15834d0a9d3a8a3cd8d6ed34e64e
fudhoil <71709328+fudhoil@users.noreply.github.com>
2022-01-28 01:03:27 +0700

    oke

--- resources/views/barang.blade.php
+++ resources/views/barang.blade.php
@@ -119,7 +119,25 @@
                   @auth
                   <td>
                     <div class="btn-group me-1">
-                      <a class="btn btn-outline-secondary rounded border-0" href="/editbarang/{editbarang}/edit"><i data-feather="edit"></i></a>
+                      <button type="button" class="btn btn-outline-secondary rounded border-0"><i data-feather="edit" data-bs-toggle="modal" data-bs-target="#ubah"></i></button>
+                      <div class="modal fade" id="ubah" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                              ...
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
                       <form action="{{ route('barang.destroy', ['barang' => $br->id]) }}" method="post">
                         @csrf
                         @method('delete')
--- resources/views/editbarang.blade.php
+++ resources/views/editbarang.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.edit')
 
 @section('container')
-    ok
+<div class="row justify-content-center mt-5">
+<div class="col-md-4">
+    <main class="form-edit">
+        <form action="/login" method="post">
+          @csrf
+          <h1 class="h3 mb-3 fw-normal">Edit data</h1>
+          <div class="form-floating">
+            <input type="text" name="namabarang" class="form-control @error('nama_barang') is-invalid @enderror" id="nama_barang" placeholder="Contoh: Sony A20" autofocus required>
+            <label for="nama_barang;">ok</label>
+            @error('email')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+          </div>
+          <div class="form-floating">
+            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
+            <label for="password">Kata Sandi</label> 
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Masuk</button>
+        </form>
+        <small class="d-block text-center mt-3">Belum terdaftar? <a href="/register">Daftar Sekarang!</a></small>
+      </main>
+</div>
+</div>
 @endsection
