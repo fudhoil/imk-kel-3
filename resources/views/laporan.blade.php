0a17b5f4d8e4c198929f901941cdfafc3aeca7f0
fudhoil <71709328+fudhoil@users.noreply.github.com>
2022-01-31 21:24:58 +0700

    aman react

--- resources/views/laporan.blade.php
+++ resources/views/laporan.blade.php
@@ -4,17 +4,9 @@
 @extends('layouts.main')
 
 @section('container')
-<<<<<<< HEAD
 
     <!-- We will put our React component inside this div. -->
     <div id="button_container"></div>
-=======
-<div class="d-flex m-5 border-box">
-    <div class="col">
-        <div class="like_button_container"></div>
-    </div>
-</div>
->>>>>>> c53559b138fa9b36ebf01b16e2f964cdd49d277a
 
 @endsection
  
