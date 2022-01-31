c53559b138fa9b36ebf01b16e2f964cdd49d277a
fudhoil <71709328+fudhoil@users.noreply.github.com>
2022-01-31 19:43:54 +0700

    ok

--- resources/views/laporan.blade.php
+++ resources/views/laporan.blade.php
@@ -4,8 +4,11 @@
 @extends('layouts.main')
 
 @section('container')
-
-Laporan
+<div class="d-flex m-5 border-box">
+    <div class="col">
+        <div class="like_button_container"></div>
+    </div>
+</div>
 
 @endsection
  
